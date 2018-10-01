4ec4350665b332dc3b87b031e47c0658f8d2de8e
Vincent Hirtz <hirtzvincent@gmail.com>
2018-10-01 22:41:43 +0200

    Please enter the commit message for your changes. Lines starting

--- .themes/default/colors.js
+++ .themes/default/colors.js
@@ -1,45 +1,8 @@
 // THEME COLORS
-const laposteYellow = "#f5c500";
 const mainColor = "#2288CC";
 const mainColorClear = "#F1FAFF";
-const secondColor = "#691268";
-
-const textColor = "#222222";
-const textColorSecondary = "#787878";
-const disabledTextColor = "#DBDBDB";
-
-const white = "#ffffff";
-const grey = "#f7f7f7";
 
 export default {
-  laposteYellow,
   mainColor,
   mainColorClear,
-  secondColor,
-  textColor,
-  textColorSecondary,
-  disabledTextColor,
-  white,
-  grey,
-
-  primaryTextColor: white,
-  secondTextColor: "#000000",
-  thirdTextColor: mainColor,
-  fourthTextColor: grey,
-
-  secondBgColor: secondColor,
-  thirdBgColor: white,
-
-  primaryBorderColor: grey,
-  secondBorderColor: "#e6e5e5",
-  thirdBorderColor: "#e3e3e3",
-
-  bodyColor: "#717171",
-
-  //MENUS
-  menuBgColor: white,
-  menuTextHoverColor: mainColor,
-
-  //Notification
-  notificationGrey: "#6c6d70",
 };
--- .themes/default/index.js
+++ .themes/default/index.js
@@ -1,24 +1,6 @@
 import button from "./button";
-import menu from "./menu";
-import modal from "./modal";
-import tooltip from "./tooltip";
-import notification from "./notification";
-import table from "./table";
-import form from "./form";
-import sliderElement from "./sliderElement";
-import breadcrumb from "./breadcrumb";
-import toggleElement from "./toggleElement";
 
 export default {
   fontFamily: "'Open Sans', sans-serif",
   button,
-  menu,
-  modal,
-  tooltip,
-  notification,
-  table,
-  form,
-  sliderElement,
-  breadcrumb,
-  toggleElement,
 };
